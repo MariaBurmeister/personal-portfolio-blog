18f0d774eea12eb8624d63bc2b389c69d001458a
MariaBurmeister <mariaburmeister@gmail.com>
2023-02-10 15:04:00 +0100

    chore(projects): filter projects

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -1,13 +1,33 @@
 "use client";
 import useSWR from "swr";
-import { ProjectsData, fetcher } from "@/pages/api/projects";
+import { useEffect, useState } from "react";
+
+const projects = [
+  "personal-portfolio-blog",
+  "reQiew_code-test",
+  "db_case-study",
+  "wo-was-denn-essen-wir-heute_code-test",
+  "codecademy_boss-machine-project",
+  "codecademy_jamming-project",
+  "codecademy_ravenous-project",
+];
 
 export const useProjects = () => {
-  const { data, error, isLoading, isValidating } = useSWR<ProjectsData, Error>(
+  const { data, error, isLoading, isValidating } = useSWR<Projects, Error>(
     "/api/projects",
     fetcher,
     { suspense: true, fallbackData: [] }
   );
+  const [filteredData, setFilteredData] = useState<Projects>();
 
-  return { data, error, isLoading, isValidating };
+  useEffect(() => {
+    if (!data) return;
+
+    const filteredData = data.filter((project) =>
+      projects.includes(project.name)
+    );
+    setFilteredData(filteredData);
+  }, [data]);
+
+  return { data: filteredData, error, isLoading, isValidating };
 };
