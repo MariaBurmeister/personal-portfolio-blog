086e3b2c6216cd64974d72a4f87f1604987243b0
MariaBurmeister <mariaburmeister@gmail.com>
2023-02-11 15:35:51 +0100

    refactor: remove unecessary complexity

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -16,30 +16,18 @@ export const Project: FunctionComponent<Project> = ({
   url,
   language,
 }) => {
-  const [expanded, setExpanded] = useState(false);
   return (
     <Card
       title={name}
       titleHelp={language}
       label={name}
       footer={<ProjectActions gitHubLink={url} />}
-      styleContent="relative flex flex-col justify-between min-h-[4.5rem] "
+      styleContent="relative  text-purple-400 p-2 rounded rounded-md min-h-[4rem] "
       styleFooter="flex justify-between items-center pr-2 pt-1"
     >
-      <p className={`line-clamp-2 ${expanded ? "line-clamp-none" : ""}`}>
+      <p className="line-clamp-2 hover:line-clamp-none">
         {description ?? name}
       </p>
-      <button
-        id="expand"
-        className="peer absolute top-0 left-0 h-full w-full opacity-0 "
-        onClick={() => setExpanded(!expanded)}
-      />
-      <label
-        htmlFor="expand"
-        className="text-xs text-purple-400 underline peer-hover:text-purple-600"
-      >
-        {expanded ? "show less" : "show more"}
-      </label>
     </Card>
   );
 };
@@ -53,7 +41,7 @@ const ProjectActions: FunctionComponent<{ gitHubLink: string }> = ({
         href=""
         className="rounded-full border  border-purple-900 bg-purple-100 px-2 py-1 shadow-sm shadow-green-400 hover:shadow-md hover:shadow-green-400"
       >
-        Expand
+        Open
       </a>
 
       <a href={gitHubLink} className="group flex items-center gap-2 text-2xl">
